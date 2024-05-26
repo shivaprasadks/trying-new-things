e321e24819379a48077c34a581415649e9e97d4f
Shivaprasad <shivaprasadks@users.noreply.github.com>
2024-05-26 07:43:11 +0000

    dependency fixes

--- src/component/fileStructure/index.jsx
+++ src/component/fileStructure/index.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-// import "./styles.css";
 import FileStructure from "./explorer";
 import { FILES_DATASET } from "./dataSet";
 
-export default function App() {
+export default function FileStructureApp() {
   const [selected, setSelectedState] = useState(null);
   const handleSelect = (item) => {
     setSelectedState(item);
